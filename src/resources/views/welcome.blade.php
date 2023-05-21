0686bffcfa677b5b9ac2abc7473e9a6d8fc88826
KizunaYuuki <time0009000@gmail.com>
2023-05-21 09:30:21 +0700

    add Bootstrap

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -8,29 +8,55 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <!-- Styles -->
         <style>
-            body {
-                display: flex;
-            }
             img {
                 width: 100%;
             }
         </style>
     </head>
     <body class="antialiased">
-        <?php
-            $users = DB::select('select * from users');
-            foreach ($users as $user) {
-                // echo $user->description;
-                $imagePath = $user->description;
-                echo    '<div> 
-                            <img src="'.$user->imagePath.'">
-                            <div>'.$user->description.'</div>
-                            <div>'.$user->imagePath.'</div>
-                        </div>';
-            }         
-        ?>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand text-danger" href="#">AVALON</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <!-- <span class="navbar-toggler-icon"></span> -->
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                    </li>   
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                    </li>                    
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Our Services</a>
+                    </li>                    
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="d-flex justify-content-center">
+            <?php
+                $users = DB::select('select * from users');
+                foreach ($users as $user) {
+                    // echo $user->description;
+                    $imagePath = $user->description;
+                    echo    '
+                                <div class="card m-2" style="width: 18rem;"> 
+                                    <img src="'.$user->imagePath.'" class="card-img-top" alt="...">
+                                    <div class="card-body">'.$user->description.'</div>
+                                </div>
+                            ';
+                }         
+            ?>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
 </html>
